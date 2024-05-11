4afd33e1c2d171018767ac331de1e39e389b2f62
bbqchickenrobot <bbqchickenrobot@lunaswap.io>
2024-05-11 07:31:33 -0500

    misc update(s)

--- BlockMountain.TradingView.Examples/ExampleExplorer.cs
+++ BlockMountain.TradingView.Examples/ExampleExplorer.cs
@@ -11,23 +11,25 @@ namespace BlockMountain.TradingView.Examples
     {
         public static void Main(string[] args)
         {
-            List<Type> examples = Assembly.GetEntryAssembly().GetExportedTypes().Where(t => t.IsAssignableTo(typeof(IRunnableExample))).ToList();
+            var examples = Assembly.GetEntryAssembly().GetExportedTypes()
+                .Where(t => t.IsAssignableTo(typeof(IRunnableExample))).ToList();
 
             while (true)
             {
                 Console.WriteLine("Choose an example to run: ");
-                int i = 0;
-                foreach (Type ex in examples)
+                var i = 0;
+                foreach (var ex in examples)
                 {
                     Console.WriteLine($"{i++}){ex.Name}");
                 }
 
-                string? option = Console.ReadLine();
+                var option = Console.ReadLine();
 
-                if (int.TryParse(option, out int res) && res <= examples.Count && res >= 0)
+                if (int.TryParse(option, out var res) && res <= examples.Count && res >= 0)
                 {
-                    Type t = examples[res];
-                    IRunnableExample example = (IRunnableExample)t.GetConstructor(Type.EmptyTypes).Invoke(null);
+                    var t = examples[res];
+                    var example = (IRunnableExample)t.GetConstructor(Type.EmptyTypes)
+                        .Invoke(null);
 
                     example.Run();
                 }
